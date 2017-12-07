394d2540f233b228cb49f4749713917354d9b2b3
bhavishyagopesh <bhavishyagopesh@gmail.com>
2017-12-07 11:04:07 +0530

    Add multiple xyt files for single entry

--- pynctual/db-operations.py
+++ pynctual/db-operations.py
@@ -8,7 +8,7 @@ def createdb():
         db = sqlite3.connect('db')
 
         cursor = db.cursor()
-        cursor.execute('''CREATE TABLE IF NOT EXISTS person (id INTEGER PRIMARY KEY AUTOINCREMENT, rollno INTEGER NOT NULL, filename TEXT)''')
+        cursor.execute('''CREATE TABLE IF NOT EXISTS person (id INTEGER PRIMARY KEY AUTOINCREMENT, rollno INTEGER NOT NULL, filename1 TEXT,filename2 TEXT,filename3 TEXT,filename4 TEXT,filename5 TEXT)''')
 
     except Exception as e:
         db.rollback()
@@ -35,9 +35,13 @@ def updatedb(roll_no):
                 pass
 
         # Ensure xyt file of intern with "rollno: 000" is named as 000.xyt
-        filename = str(roll_no)+'.xyt'
+        filename1 = str(roll_no)+'.xyt1'
+        filename2 = str(roll_no)+'.xyt2'
+        filename3 = str(roll_no)+'.xyt3'
+        filename4 = str(roll_no)+'.xyt4'
+        filename5 = str(roll_no)+'.xyt5'
         if(_cursor):
-            _cursor.execute('''INSERT INTO person (rollno, filename) VALUES(?,?)''', (roll_no, filename))
+            _cursor.execute('''INSERT INTO person (rollno, filename1, filename2, filename3, filename4, filename5) VALUES(?,?,?,?,?,?)''', (roll_no, filename1, filename2, filename3, filename4, filename5))
         else:
             raise Exception("_cursor is NULL")
 
@@ -55,7 +59,7 @@ def displaydb():
     try:
         db = sqlite3.connect("db")
         _cursor = db.cursor()
-        _rows = _cursor.execute("SELECT id, rollno, filename from person")
+        _rows = _cursor.execute("SELECT id, rollno, filename1, filename2, filename3, filename4, filename5 from person")
 
         for row in _rows:
             print('ID: {} ROLL-NO: {} FILENAME: {}'.format(row[0], row[1], row[2])) 
@@ -66,3 +70,4 @@ def displaydb():
     
     finally:
         db.close()
+
